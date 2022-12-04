9cf49d080c73a45c1a0673a3510a91213522e363
LiuCY <23275487+ChuckieEsan@users.noreply.github.com>
2022-12-04 12:44:23 +0800

    update: 更新了数据库连接方式

--- studio/__init__.py
+++ studio/__init__.py
@@ -19,22 +19,10 @@ def create_app():
     load_dotenv(override=True)
 
     # 数据库配置
-    windows_os = sys.platform.startswith("win")
-    db_user = os.getenv("MYSQL_USER")
-    db_pwd = os.getenv("MYSQL_PASSWORD")
-    db_host = os.getenv("MYSQL_HOST")
-    db_name = os.getenv("MYSQL_DATABASE")
+    db_uri = os.getenv("DB_URI")
 
-    if windows_os:
-        prefix = "sqlite:///"
-    else:
-        prefix = "sqlite:////"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    if app.debug:
-        app.config["SQLALCHEMY_DATABASE_URI"] = prefix + "../data/data.db"
-    else:
-        app.config["SQLALCHEMY_DATABASE_URI"] = \
-            f"mysql+pymysql://{db_user}:{db_pwd}@{db_host}/{db_name}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
 
     # 导入 JWT_KEY
     app.config["JWT_KEY"] = os.getenv("JWT_KEY")
