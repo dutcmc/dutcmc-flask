6a8167ee413e0a6d40f00ecd1f401519fabdffed
LiuCY <23275487+ChuckieEsan@users.noreply.github.com>
2023-03-09 09:21:06 +0800

    update: 1. 添加了新版WCI计算功能的相关接口

--- studio/api/apivue/wci.py
+++ studio/api/apivue/wci.py
@@ -1,9 +1,16 @@
 from flask import Blueprint, request
 import pandas as pd
 import numpy as np
-import os
+import os, re
 
 wci = Blueprint("wci", __name__, url_prefix="/wci")
+
+
+def convert(value):
+    if str(value).find("W") > -1:  # 如果找到 W，则进行转换
+        return int(re.findall("[0-9]+", str(value))[0]) * 10000
+    else:
+        return value
 
 
 def calcWCI(R: [], Z: [], L: [], Rt: int, Zt: int, Lt: int, d: int):
@@ -14,6 +21,25 @@ def calcWCI(R: [], Z: [], L: [], Rt: int, Zt: int, Lt: int, d: int):
     tR = np.sum(R)
     tZ = np.sum(Z)
     tL = np.sum(L)
+
+    O = 0.85 * np.log(tR / d + 1) + 0.09 * np.log(tZ / d * 10 + 1) + 0.06 * np.log(tL / d * 10 + 1)
+    A = 0.85 * np.log(tR / n + 1) + 0.09 * np.log(tZ / n * 10 + 1) + 0.06 * np.log(tL / n * 10 + 1)
+    H = 0.85 * np.log(Rt / d + 1) + 0.09 * np.log(Zt / d * 10 + 1) + 0.06 * np.log(Lt / d * 10 + 1)
+    P = 0.85 * np.log(mR + 1) + 0.09 * np.log(mZ * 10 + 1) + 0.06 * np.log(mL * 10 + 1)
+    return np.power(0.6 * O + 0.2 * A + 0.1 * H + 0.1 * P, 2) * 12
+
+
+def calcWCIEx(tR: int, tZ: int, tL: int, mR: int, mZ: int, mL: int, Rt: int, Zt: int, Lt: int, d: int, n: int, lstR: [],
+              lstZ: [], lstL: [], lstW: []):
+    n = len(lstR) + n  # 变更后的文章总数
+    tR = tR + np.sum([v * w / 100 for v, w in zip(lstR, lstW)])  # 变更后的总阅读量
+    tZ = tZ + np.sum([v * w / 100 for v, w in zip(lstZ, lstW)])  # 变更后的总在看数
+    tL = tL + np.sum([v * w / 100 for v, w in zip(lstL, lstW)])  # 变更后的总点赞数
+    mR = np.max([mR] + [v * w / 100 for v, w in zip(lstR, lstW)])  # 变更后的阅读量最大值
+    mZ = np.max([mZ] + [v * w / 100 for v, w in zip(lstZ, lstW)])  # 变更后的在看数最大值
+    mL = np.max([mL] + [v * w / 100 for v, w in zip(lstL, lstW)])  # 变更后的点赞数最大值
+    # 注意, Rt, Zt, Lt 将不考虑，即所有的附加文章都不会视作头条
+    print(n, tR, tZ, tL, mR, mZ, mL, Rt, Zt, Lt)
 
     O = 0.85 * np.log(tR / d + 1) + 0.09 * np.log(tZ / d * 10 + 1) + 0.06 * np.log(tL / d * 10 + 1)
     A = 0.85 * np.log(tR / n + 1) + 0.09 * np.log(tZ / n * 10 + 1) + 0.06 * np.log(tL / n * 10 + 1)
@@ -74,4 +100,56 @@ def r_calc_wci():
         L = totalPassages["L"].to_numpy()
         result.append({"account": name, "wci": calcWCI(R, Z, L, Rt, Zt, Lt, d)})
     result = sorted(result, key=lambda x: x["wci"], reverse=True)
+    return {"success": True, "data": result}
+
+
+@wci.route("/parseDataEx", methods=["POST"])
+def r_parse_data_ex():
+    file = request.files.getlist("file")[0]  # 仅接收一个文件
+    if not os.path.exists("tmp"):
+        os.mkdir("tmp")
+    filename = file.filename
+    file.save(f"tmp/{filename}")
+    data = pd.read_excel(f"tmp/{filename}", skipfooter=1).applymap(convert)
+    accounts = []
+    result = []
+    for _, row in data.iterrows():
+        accounts.append({
+            "value": row["公众号"],
+            "label": row["公众号"]
+        })
+        result.append({
+            "account": row["公众号"],
+            "n": row["文章总数"],
+            "R": row["阅读总数"],
+            "Z": row["在看总数"],
+            "L": row["点赞总数"],
+            "Rm": row["最大阅读数"],
+            "Zm": row["最大在看数"],
+            "Lm": row["最大点赞数"],
+            "Rt": row["头条文章阅读量"],
+            "Zt": row["头条文章在看数"],
+            "Lt": row["头条文章点赞总数"],
+            "wci": row["WCI"]
+        })
+    os.remove(f"tmp/{filename}")
+    return {"success": True, "data": result, "accounts": accounts}
+
+
+@wci.route("/calcWCIEx", methods=["POST"])
+def r_calc_wci_ex():
+    result = []
+    lstDictReq = request.get_json()["data"]
+    d = request.get_json()["d"]
+    for value in lstDictReq:
+        relatePushes = value["relatePushes"]
+        df = pd.DataFrame(relatePushes)
+        if df.shape[0] > 0:
+            result.append({"account": value["account"], "wci":
+                calcWCIEx(value["R"], value["Z"], value["L"], value["Rm"], value["Zm"], value["Lm"],
+                          value["Rt"], value["Zt"], value["Lt"], d, value["n"],
+                          df["R"].to_list(), df["Z"].to_list(), df["L"].to_list(), df["weight"].to_list())})
+        else:
+            result.append({"account": value["account"], "wci": value["wci"]})
+
     return {"success": True, "data": result}
